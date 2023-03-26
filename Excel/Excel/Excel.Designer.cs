496fddc32bca219d0a0f70b0e416c0b109cc9ad2
Sze Wong Liaw <liawsw@live.com>
2023-03-26 16:16:43 +0800

    Completed load and save

--- Excel/Excel/Excel.Designer.cs
+++ Excel/Excel/Excel.Designer.cs
@@ -28,13 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgView = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.openFD = new System.Windows.Forms.OpenFileDialog();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgView
+            // 
+            this.dgView.AllowUserToAddRows = false;
+            this.dgView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dgView.Location = new System.Drawing.Point(73, 47);
+            this.dgView.Name = "dgView";
+            this.dgView.RowHeadersVisible = false;
+            this.dgView.Size = new System.Drawing.Size(661, 275);
+            this.dgView.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column1.HeaderText = "No";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 46;
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column2.HeaderText = "UserID";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 65;
+            // 
+            // Column3
+            // 
+            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column3.HeaderText = "Password";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 78;
+            // 
+            // Column4
+            // 
+            this.Column4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column4.HeaderText = "Remarks";
+            this.Column4.Name = "Column4";
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(292, 365);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // openFD
+            // 
+            this.openFD.FileName = "Users.xlsx";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(392, 365);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Excel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.dgView);
+            this.Name = "Excel";
+            this.Text = "Excel";
+            ((System.ComponentModel.ISupportInitialize)(this.dgView)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgView;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.OpenFileDialog openFD;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.Button btnSave;
     }
 }
 
--- Excel/Excel/Excel.cs
+++ Excel/Excel/Excel.cs
@@ -4,17 +4,103 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
+//https://www.c-sharpcorner.com/UploadFile/ankurmee/import-data-from-excel-to-datagridview-in-C-Sharp/https://www.c-sharpcorner.com/UploadFile/ankurmee/import-data-from-excel-to-datagridview-in-C-Sharp/
+//https://www.freecodespot.com/blog/csharp-import-excel/
+//https://www.youtube.com/watch?v=LDq4C_wF0fs
+//https://www.c-sharpcorner.com/UploadFile/hrojasara/export-datagridview-to-excel-in-C-Sharp/
 
 namespace Excel
 {
     public partial class Excel : Form
     {
+        string filename;
+
+        Microsoft.Office.Interop.Excel.Application xlsApp;
+        Microsoft.Office.Interop.Excel.Workbook xlsWorkbook;
+        Microsoft.Office.Interop.Excel.Worksheet xlsWorkSheet;
+        Microsoft.Office.Interop.Excel.Range xlsRange;
+
         public Excel()
         {
             InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            openFD.Filter = "Excel Office | *.xls; *.xlsx";
+            var result =  openFD.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                filename = openFD.FileName;
+                try
+                {
+                    xlsApp = new Microsoft.Office.Interop.Excel.Application();
+                    xlsWorkbook = xlsApp.Workbooks.Open(filename);
+                    xlsWorkSheet = xlsWorkbook.Worksheets["Users"];
+                    xlsRange = xlsWorkSheet.UsedRange;
+
+                    for (int row = 2; row <= xlsRange.Rows.Count; row++)
+                    {
+                        dgView.Rows.Add(xlsRange.Cells[row,1].Text, xlsRange.Cells[row, 2].Text, xlsRange.Cells[row, 3].Text, xlsRange.Cells[row, 4].Text);
+                    }
+
+                    GC.Collect();
+                    GC.WaitForPendingFinalizers();
+                    Marshal.ReleaseComObject(xlsRange);
+                    Marshal.ReleaseComObject(xlsWorkSheet);
+                    //quit apps
+                    xlsWorkbook.Close();
+                    Marshal.ReleaseComObject(xlsWorkbook);
+                    xlsApp.Quit();
+                    Marshal.ReleaseComObject(xlsApp);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    MessageBox.Show("Check Office x86 or x64.  Must be compatible with App");
+                }
+            }
+
+            
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            xlsApp = new Microsoft.Office.Interop.Excel.Application();
+            xlsWorkbook = xlsApp.Workbooks.Open(filename);
+            xlsWorkSheet = xlsWorkbook.Worksheets["Users"];
+            xlsRange = xlsWorkSheet.UsedRange;
+
+            // storing Each row and column value to excel sheet  
+            for (int i = 0; i < dgView.Rows.Count; i++)
+            {
+                for (int j = 0; j < dgView.Columns.Count; j++)
+                {
+                    xlsRange.Cells[i + 2, j + 1] = dgView.Rows[i].Cells[j].Value.ToString();
+                }
+            }
+            // save the application  
+            //xlsWorkbook.SaveAs("C:\\Users\\liaws\\Desktop\\test.xlsx", Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlExclusive, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+            xlsWorkbook.SaveAs("C:\\Users\\liaws\\Desktop\\test.xlsx", Microsoft.Office.Interop.Excel.XlFileFormat.xlOpenXMLWorkbook, System.Reflection.Missing.Value, Missing.Value, false, false, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlNoChange, Microsoft.Office.Interop.Excel.XlSaveConflictResolution.xlUserResolution, true, Missing.Value, Missing.Value, Missing.Value);
+            xlsWorkbook.Saved = true;
+
+            MessageBox.Show("File Updated");
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            Marshal.ReleaseComObject(xlsRange);
+            Marshal.ReleaseComObject(xlsWorkSheet);
+            //quit apps
+            xlsWorkbook.Close();
+            Marshal.ReleaseComObject(xlsWorkbook);
+            xlsApp.Quit();
+            Marshal.ReleaseComObject(xlsApp);
         }
     }
 }
